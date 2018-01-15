24bd7a949d5111c601fb29ab765e4ab9ad0bb52b
Cecilia Wong <cissi.wong@gmail.com>
2018-01-15 13:02:53 +0100

    added token to model and set up authentication

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose"
 import express from "express"
 import bodyParser from "body-parser"
 import uuid from "uuid/v4"
+import bcrypt from "bcrypt-nodejs"
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -20,21 +21,45 @@ mongoose.connection.once("open", () => {
 })
 
 const User = mongoose.model("User", {
-  name: String,
-  accessToken: { type: String, default: () => uuid() }
+  name: {
+    type: String,
+    unique: true
+  },
+  password: String,
+  accessToken: {
+    type: String,
+    default: () => uuid()
+  }
 })
-
-// const firstUser = new User({ name: "Bob" })
+//
+// const firstUser = new User({ name: "Bob", password: bcrypt.hashSync("foobar") })
 // firstUser.save().then(() => console.log("Created Bob"))
 //
-// const secondUser = new User({ name: "Sue" })
+// const secondUser = new User({ name: "Sue", password: bcrypt.hashSync("password1") })
 // secondUser.save().then(() => console.log("Created Sue"))
 
-app.get("/users/:id", (req, res) => {
-  res.json({
-    requestingUserId: req.params.id,
-    requestToken: req.headers.token
+const findUser = (req, res, next) => {
+  User.findById(req.params.id).then(user => {
+    // res.json(user)
+    if (user.accessToken === req.headers.token) {
+      req.user = user
+      next()
+    } else {
+      res.status(401).send("unauthenticated")
+    }
   })
+}
+// mounting middleware //
+app.use("/users/:id", findUser)
+
+app.get("/users/:id", (req, res) => {
+  res.json(req.user)
+})
+
+app.use("/hello/:id", findUser)
+
+app.get("/hello/:id", (req, res) => {
+  res.json({ hello: req.user.name })
 })
 
 app.listen(8080, () => console.log("Example app listening on port 8080!"))
